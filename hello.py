ac806b49df4ca4a94bb0d2fa3fc277c2ce5b35bc
rtyUM333 <rtypien@gmail.com>
2024-08-14 21:33:16 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-# ask user for their name
-name = input("What's your name? ")
+# ask user for their name (also remove white space from str and capitalize user's name)
+name = input("What's your name? ").strip().title()
+
 
 #say hello to user
 print (f"hello, {name}")
+
