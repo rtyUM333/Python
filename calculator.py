ad88495d63043182b83af8fb60e31cdfc8d0a657
rtyUM333 <rtypien@gmail.com>
2024-08-27 13:26:42 -0500

    learned creating functions with "def"

--- calculator.py
+++ calculator.py
@@ -1,6 +1,21 @@
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+# x = float(input("What's x? "))
+# y = float(input("What's y? "))
 
-z = round (x+y)
+# z = round (x+y)
 
-print (z)
+# to round integer 
+# z = round (x / y, 2)
+
+# z = x / y
+
+# this is the way you specify in f string how many digits you wanna round up to 
+# print (f"{z:.2f}")
+
+def main():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n
+
+main()
--- hello.py
+++ hello.py
@@ -1,7 +1,13 @@
-# ask user for their name (also remove white space from str and capitalize user's name)
-name = input("What's your name? ").strip().title()
-
-
+#ask user for their name (also remove white space from str and capitalize user's name)
+#name = input("What's your name? ").strip().title()
 #say hello to user
-print (f"hello, {name}")
+#print (f"hello, {name}")
 
+
+#creating function, using keyword "def" = define
+# def hello(to="world"): #the equal after the to is a DEFAULT value
+#     print("hello,", to)
+
+# hello() # this will print hello world, as default
+# name = input("What is your name? ")
+# hello(name)
