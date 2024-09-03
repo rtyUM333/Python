36da0d61229e08afe8bd0790866b0768e0cd9d03
rtyUM333 <rtypien@gmail.com>
2024-09-03 12:13:49 -0500

    Conditionals in Python

---
+++ compare.py
@@ -0,0 +1,23 @@
+x = int(input("What's x? "))
+y = int(input("what's y? "))
+
+#boolean expression - a question that has y/n or t/f answers
+
+# if x < y:
+#     print("x is less than y") 
+# elif x > y:
+#     print("x is greater than y")
+# else:
+#     print("x is equal to y")
+
+
+# if x < y or x > y:
+#     print("x is not equal to y")
+# else:
+#     print("x is equal to y")
+
+
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
---
+++ parity.py
@@ -0,0 +1,1 @@
+x = int(input("What's x? "))
